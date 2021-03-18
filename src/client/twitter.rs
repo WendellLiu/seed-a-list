2a12c706399f80cedf1fa03ceec598f3d285294d
Wendell Liu <cuk.bas@gmail.com>
2021-03-18 22:45:10 +0800

    feat: request successfully

--- src/client/twitter.rs
+++ src/client/twitter.rs
@@ -1,1 +1,14 @@
-use reqwest::get
+use std::future::Future;
+
+use reqwest::Client;
+
+pub async fn foo() -> Result<String, reqwest::Error> {
+    let client = Client::new();
+
+    client
+        .get("https://api.twitter.com/2/users/4781015496/mentions")
+        .send()
+        .await?
+        .text()
+        .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+mod client;
 mod config;
 
+use client::twitter::foo;
 use config::{SystemConfig, SYSTEM_CONFIG};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     let system_config_instance = SystemConfig::new();
     SYSTEM_CONFIG.set(system_config_instance).unwrap();
 
     let system_config = SystemConfig::global();
 
-    println!("system config: {}", system_config.twitter.token)
+    println!("system config: {}", system_config.twitter.token);
+
+    let resp = foo().await?;
+
+    println!("{:#?}", resp);
+    Ok(())
 }
