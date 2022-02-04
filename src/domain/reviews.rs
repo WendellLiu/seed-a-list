0c441f9be0a80fac3db88295ebb542ba9b2337f5
Wendell Liu <cuk.bas@gmail.com>
2022-02-05 00:59:34 +0800

    feat: parse tags

--- src/domain/reviews.rs
+++ src/domain/reviews.rs
@@ -1,8 +1,20 @@
 use std::sync::Arc;
 
+use regex::Regex;
+
 use crate::client::twitter::{MentionsResponse, Tweet};
 use crate::models::reviews::t::Source;
 use crate::repository::reviews::{InsertError, Repository};
+
+fn parse_tags(content: &String) -> Vec<String> {
+    let re = Regex::new(r"(#(?P<hashtag>[a-z\d-]+))").unwrap();
+    let mut hashtags = Vec::new();
+    for caps in re.captures_iter(content) {
+        hashtags.push(String::from(&caps["hashtag"]));
+    }
+
+    hashtags
+}
 
 fn create_review(repo: Arc<dyn Repository>, tweet: &Tweet) {
     match repo.insert(&tweet.author_id, &tweet.id, Source::Twitter, &tweet.text) {
@@ -18,4 +30,17 @@ pub fn create_reviews(repo: Arc<dyn Repository>, resp: MentionsResponse) {
             .for_each(|tweet| create_review(repo.clone(), tweet)),
         None => (),
     };
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_should_parse_tags() {
+        let tags = parse_tags(&String::from(
+            "https://example.com/music1 \n it's a great song! #happy and #upset",
+        ));
+        assert_eq!(tags, vec!["happy", "upset"]);
+    }
 }
