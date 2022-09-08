0a02cc3567e3d5aee9e5a8c587315acf728ec746
Wendell Liu <wendell.liu@houzz.com>
2022-09-08 18:14:47 +0800

    feat: back to develop

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 mod client;
 mod config;
-//mod db;
-//mod domain;
-//mod models;
-//mod repository;
-//mod schema;
+mod db;
+mod domain;
+mod models;
+mod repository;
+mod schema;
 
 #[macro_use]
 extern crate diesel;
 
-//use std::sync::Arc;
+use std::sync::Arc;
 
-//use client::twitter::TwitterClient;
+use client::twitter::TwitterClient;
 use config::{SystemConfig, SYSTEM_CONFIG};
-//use db::pool::establish_pool;
-//use domain::reviews::create_reviews;
-//use repository::reviews::MysqlRepository;
+use db::pool::establish_pool;
+use domain::reviews::create_reviews;
+use repository::reviews::MysqlRepository;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -25,15 +25,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let system_config = SystemConfig::global();
 
     println!("{:#?}", &system_config);
-    //let twitter_client = TwitterClient::new(&system_config.twitter.token);
+    let twitter_client = TwitterClient::new(&system_config.twitter.token);
 
-    //let resp = twitter_client
-    //.get_mentions(system_config.twitter.official_account_id, 100)
-    //.await?;
-    //println!("{:#?}", resp);
+    let resp = twitter_client
+        .get_mentions(system_config.twitter.official_account_id, 100)
+        .await?;
+    println!("{:#?}", resp);
 
-    //let pool = establish_pool(&system_config.mysql.endpoint);
-    //let repo = MysqlRepository { pool };
+    let pool = establish_pool(&system_config.mysql.endpoint);
+    let repo = MysqlRepository { pool };
     //create_reviews(Arc::new(repo), resp);
 
     Ok(())
